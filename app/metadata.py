1f0005f3cbcf1f1ea33c5af6bb1dff6ce1eb2269
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2022-01-07 12:42:45 -0500

    # fether format

--- app/metadata.py
+++ app/metadata.py
@@ -3,6 +3,10 @@ metadata.py
     -- basic service for metadata 
 """
 
+import os
+import pandas as pd
+import geopandas as gpd
+
 from flask import Blueprint, jsonify
 from flask import current_app
 
@@ -11,4 +15,37 @@ metadata = Blueprint("metadata",__name__)
 @metadata.route("/")
 def metadata_home():
     metajson = current_app.config['METADATA']
-    return jsonify(metadata=metajson)
+    status = check_metajson(metajson)
+
+    desc = {"metadata":metajson, "status":status}
+
+    return jsonify(desc)
+
+@metadata.route('/<uid>')
+def dashboard(uid):
+   return jsonify(uid=uid)
+
+def check_metajson(metadata):
+    """check metajson file"""
+
+    if not os.path.exists(metadata):
+        return "not found"
+    
+    status = load_metajson(metadata)
+
+    return status
+
+def load_metajson(metadata):
+    """load metadata from .geojsonfile"""
+
+    feather = metadata.replace(".geojson",".feather")
+    try:
+        data = gpd.read_feather(feather)
+    except FileNotFoundError:
+        # feature is not found
+        # load geosjon file and generate feather
+        data = gpd.read_file(metadata)
+        data.to_feather(feather)
+        return "load geojson file"
+    
+    return "load feather file"
