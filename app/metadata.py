2b253c3d799a4a507073178af13230db0b1da102
webgisdeveloper <webgisdeveloper@users.noreply.github.com>
2022-01-19 15:53:33 -0500

    search by geometry

--- app/metadata.py
+++ app/metadata.py
@@ -10,7 +10,7 @@ import geopandas as gpd
 from flask import Blueprint
 from flask import current_app
 from flask import request
-from shapely.geometry.polygon import orient
+from shapely.geometry import * 
 
 metadata = Blueprint("metadata",__name__)
 
@@ -101,6 +101,18 @@ def search_uavsar_flightname(sdata,flightname):
     
     return data
 
+def search_uavsar_geometry(sdata,geometry):
+    """search by geometry"""
+
+    # sample gemetry input
+    # Point: (33.94697109585554, -118.07714843749999)
+    # Rectangle: ((33.343343561567, -118.69238281249999), (34.34703015733175, -117.04443359374999))
+    # Line: (34.72713264415401, -119.37353515624999),(33.67311969894201, -117.76953124999999),(33.983418330994276, -116.20947265624999),(32.4021911686893, -116.91259765624999)
+    # Polygon: (34.05626592724434, -117.59374999999999),(33.572488142047554, -118.38476562499999),(32.837100401791204, -117.25317382812499),(33.4809029903106, -116.73681640624999)
+    data = sdata
+
+    return data
+
 def search_uavsar(searchdict):
     """search UAVSAR """
     metajson = current_app.config['METADATA']
@@ -110,4 +122,7 @@ def search_uavsar(searchdict):
     if len(searchdict['flightname']) >=1:
         searchdata = search_uavsar_flightname(searchdata,searchdict['flightname'])
     
+    if len(searchdict['geometry']) >=1:
+        searchdata = search_uavsar_geometry(searchdata,searchdict['geometry'])
+        
     return searchdata
